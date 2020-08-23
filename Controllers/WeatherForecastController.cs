cf45df4631d129802c7bf5044d15b8d7197bfb4d
pasanjg <44201430+pasanjg@users.noreply.github.com>
2020-08-24 03:18:51 +0530

    Create Controller with DBContext

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace BlogAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
