f15c5dd391265927a3a23a479d946c0c37302109
pasanjg <44201430+pasanjg@users.noreply.github.com>
2020-09-04 19:14:02 +0530

    Remove unwanted imports

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using BlogAPI.Data;
 using BlogAPI.Models;
 using BlogAPI.Services;
 
